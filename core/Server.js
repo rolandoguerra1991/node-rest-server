9c64c3a42f6c37c8d29587cbff34f34a61f615e8
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-09-20 21:39:23 -0400

    added dynamic routes

--- core/Server.js
+++ core/Server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 class Server {
   constructor() {
+    this.routerPath = '../routes';
+    this.prefixUrl = '/v1/api';
     this.app = express();
     this.port = process.env.PORT;
     this.middleWares();
@@ -10,7 +14,16 @@ class Server {
   }
 
   routes() {
-    this.app.use('/api/users', require('../routes/users.router'));
+    fs.readdir(path.join(__dirname, this.routerPath), (err, files) => {
+      if (!err) {
+        files.forEach((file) => {
+          const url = `${this.prefixUrl}/${file.split('.')[0]}/`;
+          const route = `${this.routerPath}/${file}`;
+          console.log(url);
+          this.app.use(url, require(route));
+        })
+      }
+    })
   }
 
   middleWares() {
